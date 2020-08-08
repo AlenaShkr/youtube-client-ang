f31a67d3d1c837877647e0b9a5623b05f2f08e46
AlenaShkr <elalshkr@gmail.com>
2020-08-09 02:12:58 +0300

    youtube-client-ang/feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchRequestComponent } from './components/search-request/search-request.component';
+import { SortCriteriaListComponent } from './components/sort-criteria-list/sort-criteria-list.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
+import { ResultCardComponent } from './components/result-card/result-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchRequestComponent,
+    SortCriteriaListComponent,
+    ResultListComponent,
+    ResultCardComponent
   ],
   imports: [
     BrowserModule,
